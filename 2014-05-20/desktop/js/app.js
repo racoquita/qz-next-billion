75d9b4ec3d7d89aebd18c25fbc1752ca2f65748f
Richard Stovall <rstovall@me.com>
2014-05-20 21:20:28 -0400

    making the circles generate automagically on an interval

--- 2014-05-20/desktop/js/app.js
+++ 2014-05-20/desktop/js/app.js
@@ -9,27 +9,31 @@ var App = function() {
 	var interval;
 	var to=[];
 	this.on = function() {
-		cta.on('click', that.ctaClickHandler)
+		cta.on('click', that.ctaClickHandler);
+
+		that.createCircle();
+		interval = setInterval(function(){
+			that.createCircle();
+		}, 3000);
 	}
 	this.off = function() {
-		that.resetAd();
-	}
-	this.upnum = function (number) {
-		
-		return number+1
-	}
-	this.resetAd = function(){
 		num = 3;
+		
+		$.each(to, function(i, tout) {
+			clearTimeout(tout)
+		});
+		
+		clearInterval(interval);
+
 		$('.frame').addClass('hide').hide();
 		$('.frame3').find('img.chart').attr('src', 'images/f'+num+'-chart.png')
 		$('.frame1').removeClass('hide').show();
-		$.each(to, function(i, tout) {
-			clearTimeout(tout)
-		})
+		$('.circle').remove();
+	}
+	this.upnum = function (number) {
+		return number+1
 	}
 	this.createCircle = function() {
-		
-
 		$('.circle').remove();
 
 		for(var i = 0; i < 3; i++) {
@@ -45,34 +49,27 @@ var App = function() {
 				},290);
 			}, 250 * i)
 		}
-		
 	}
 	this.ctaClickHandler = function(e) {
-		
-
 		for (var i = 4; i < 8; i++) {
 			imgs[i] = 'images/f'+i+'-chart.png';
 		};
+
 		currentFrame = $(e.target).parents('.frame');
 		var goTo = $(e.target).closest('a').attr('href');
 		
-
 		if (goTo == 'frame4') {
 			num = that.upnum(num);
 			var chart = $(e.target).parent('.frame3').find('img.chart').attr('src', imgs[num]);
-			
 
 			if(num == 8){
 				$('.circle').remove();
 				currentFrame.addClass('hidden').hide();
 				$('.cta-bg, .circle').hide();
 				$('.'+goTo).fadeIn(250).removeClass('hidden');
-			}else if(num < 8){
-				that.createCircle();
 			}
 			
-		}else{
-			that.createCircle();
+		} else {
 			currentFrame.addClass('hidden').hide();
 			$('.'+goTo).fadeIn('250').removeClass('hidden');
 		}
--- 2014-05-20/mobile/js/app.js
+++ 2014-05-20/mobile/js/app.js
@@ -9,23 +9,32 @@ var App = function() {
 	var interval;
 	var to=[];
 	this.on = function() {
-		cta.on('click', that.ctaClickHandler)
+		cta.on('click', that.ctaClickHandler);
+
+		that.createCircle();
+		interval = setInterval(function(){
+			that.createCircle();
+		}, 3000);
 	}
 	this.off = function() {
-		that.resetAd();
-	}
-	this.upnum = function (number) {
-		
-		return number+1
-	}
-	this.resetAd = function(){
 		num = 3;
+
+		$.each(to, function(i, tout) {
+			clearTimeout(tout)
+		});
+
+		clearInterval(interval);
+		
 		$('.frame').addClass('hide').hide();
 		$('.frame3').find('img.chart').attr('src', 'images/f'+num+'-chart.png')
 		$('.frame1').removeClass('hide').show();
-		$.each(to, function(i, tout) {
-			clearTimeout(tout)
-		})
+		$('.circle').remove();
+	}
+	this.upnum = function (number) {
+		return number+1
+	}
+	this.resetAd = function(){
+		
 	}
 	this.createCircle = function() {
 		
@@ -48,11 +57,10 @@ var App = function() {
 		
 	}
 	this.ctaClickHandler = function(e) {
-		
-
 		for (var i = 4; i < 8; i++) {
 			imgs[i] = 'images/f'+i+'-chart.png';
 		};
+
 		currentFrame = $(e.target).parents('.frame');
 		var goTo = $(e.target).closest('a').attr('href');
 		
@@ -61,18 +69,14 @@ var App = function() {
 			num = that.upnum(num);
 			var chart = $(e.target).parent('.frame3').find('img.chart').attr('src', imgs[num]);
 			
-
 			if(num == 8){
 				$('.circle').remove();
 				currentFrame.addClass('hidden').hide();
 				$('.cta-bg, .circle').hide();
 				$('.'+goTo).fadeIn(250).removeClass('hidden');
-			}else if(num < 8){
-				that.createCircle();
 			}
 			
-		}else{
-			that.createCircle();
+		} else {
 			currentFrame.addClass('hidden').hide();
 			$('.'+goTo).fadeIn('250').removeClass('hidden');
 		}
